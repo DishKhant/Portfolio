88fe396617b86111f9a88b880bdaeabd3c723f78
Dish khant <khantdish9@gmail.com>
2024-12-13 20:26:20 +0530

    Change title color.

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -87,7 +87,7 @@ const Projects = () => {
               </div>
 
               <div className="p-6">
-                <h3 className="text-xl font-semibold text-gray-900 mb-2">{project.title}</h3>
+                <h3 className="text-xl font-semibold text-indigo-500 mb-2">{project.title}</h3>
                 <p className="text-gray-600 mb-4">{project.description}</p>
                 <div className="flex flex-wrap gap-2">
                   {project.tags.map((tag, i) => (
